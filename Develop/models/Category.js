576f226e314efcf33e1e7097394deac0e16f086f
Jason Viox <jrv04@hotmail.com>
2021-08-04 15:01:57 -0500

    defined category columns

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,7 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
   },
+  category_name: {
+    type: DataTypes.VARCHAR,
+    notNull: true,
+  },
+},
   {
     sequelize,
     timestamps: false,
