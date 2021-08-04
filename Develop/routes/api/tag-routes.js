33128e5e70f1b8750314d3678551fb42e8b4316a
Jason Viox <jrv04@hotmail.com>
2021-08-04 14:28:11 -0500

    start tag-routes file

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
+  ProductTag.findAll().then((productTagData) => {
+    res.json(productTagData);
+  });
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Category.findAll({
   // be sure to include its associated Product data
 });
 
