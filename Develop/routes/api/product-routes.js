e9429c72f923fb8f48c0618fb7a83dae50dffc89
Jason Viox <jrv04@hotmail.com>
2021-08-04 14:18:12 -0500

    add get function

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,14 +6,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll().then((productData) => {
+    res.json(productData);
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findAll({
+
+    order: [''],
+    where: {
   // be sure to include its associated Category and Tag data
-});
+    is_something: true
+    },
+  }).then(productData) => {
+    res.json(productData);
+};
 
 // create new product
 router.post('/', (req, res) => {
