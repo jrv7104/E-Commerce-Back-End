988ed1193f58c8d1e3cb30b79a339c414ef8d30f
Jason Viox <jrv04@hotmail.com>
2021-08-09 15:28:02 -0500

    update get post routes in product and tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,53 +1,32 @@
-// const router = require('express').Router();
-// const { Tag, Product, ProductTag, Category } = require('../../models');
+const router = require('express').Router();
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // // The `/api/tags` endpoint
 
-// router.get('/', (req, res) => {
-//   // find all tags
-//   ProductTag.findAll().then((productTagData) => {
-//     res.json(productTagData);
-//   });
-//   // be sure to include its associated Product data
-// });
 
-// router.get('/:id', (req, res) => {
-//   // find a single tag by its `id`
-//   Category.findAll({
-//   // be sure to include its associated Product data
+router.get('/', (req, res) => {
+    Tag.findAll({
+        include: [Category, Product]
+      }).then(product => res.json(product))
+    });
 
-//   order: [''],
-//   where: {
+router.get('/:id', (req, res) => {
+    // find one category by its `id` value
+    // // be sure to include its associated Products
+    
+    Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Product, Category]
+    }).then(tags => res.json(tags))
+  });
 
-//     is_something: true
-//   },
-// }).then(productTagData) => {
-//   res.json(productTagData);
-// };
+router.post('/', (req, res) => {
 
-// router.post('/', (req, res) => {
+    Product.create(req.body).then(newProduct => res.json(newProduct))
+});
 //   // create a new tag
-// try {
-//   const newTag = await Tag.create({
-//     tag_name: req.body.tag_name,
-//     price: req.body.price,
-//     stock: req.body.stock,
-//     product_name: req.body.product_name,
-//     category_name: req.body.category_name,
-//   });
-
-//   res.status(200).json(newTag);
-// } catch (err) {
-//   res.status(400).json(err);
-
-// //   Product.create(req.body)
-// //   .then(newProductTag) => {
-// //     res.json(newProductTag);
-// //   })
-// //   .catch((err) => {
-// //     res.json(err);
-// //   });
-// // });
 
 // router.put('/:id', (req, res) => {
 //   // update a tag's name by its `id` value
